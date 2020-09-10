e95457e40d98cf9a073be8771376b54aac1cbd53
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-09-10 13:17:33 -0400

    left of

--- Javascript/request.js
+++ Javascript/request.js
@@ -1,4 +1,6 @@
 const key = "802bea7637711ef757433b79e8575bb3";
 
 const baseURL =
-  "api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}";
+  "http://api.openweathermap.org/data/2.5/weather?q={city name}&appid=802bea7637711ef757433b79e8575bb3";
+// https://www.youtube.com/watch?v=QDdn3yrsyCQ
+// left of at minute 31:48
