dea277940e0f96955ab469d6b2be58e83b74a08e
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-09-14 13:44:41 -0400

    api

--- Javascript/request.js
+++ Javascript/request.js
@@ -1,6 +1,10 @@
 const key = "802bea7637711ef757433b79e8575bb3";
 
 const baseURL =
-  "http://api.openweathermap.org/data/2.5/weather?q={city name}&appid=802bea7637711ef757433b79e8575bb3";
+  "http://api.openweathermap.org/data/2.5/weather?q=Charlotte&appid=802bea7637711ef757433b79e8575bb3";
 // https://www.youtube.com/watch?v=QDdn3yrsyCQ
 // left of at minute 31:48
+
+fetch(baseURL).then((data) => {
+  console.log("response", data);
+});
