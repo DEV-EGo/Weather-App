c7fc6b2c11013590a340374df04151af2cf55a46
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-09-14 14:16:57 -0400

    async

--- Javascript/request.js
+++ Javascript/request.js
@@ -1,10 +1,21 @@
 const key = "802bea7637711ef757433b79e8575bb3";
 
-const baseURL =
-  "http://api.openweathermap.org/data/2.5/weather?q=Charlotte&appid=802bea7637711ef757433b79e8575bb3";
-// https://www.youtube.com/watch?v=QDdn3yrsyCQ
-// left of at minute 31:48
+// const baseURL =
+//   "http://api.lopenweathermap.org/data/2.5/weather?q=Lagos&appid=802bea7637711ef757433b79e8575bb3";
 
-fetch(baseURL).then((data) => {
-  console.log("response", data);
-});
+// fetch(baseURL)
+//   .then((data) => {
+//     console.log("response", data.json());
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+const requestCity = async (city) => {
+  const baseURL = "http://api.lopenweathermap.org/data/2.5/weather";
+  const query = `?q=${city}appid=${key}`;
+
+  // make fetch call (promise call)
+
+  const response = await fetch(baseURL + query);
+};
